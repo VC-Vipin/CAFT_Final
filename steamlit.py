5508496b00695684b7147c4e7538809a7ca862a8
VC-Vipin <vipinkumar.rishikesh@gmail.com>
2025-03-21 12:43:32 +0530

    Add files via upload

---
+++ steamlit.py
@@ -0,0 +1,14 @@
+import streamlit as st
+st.title ("streamlit App")
+st.header("This is header")
+st.subheader("This is Subheader")
+st.text("CAFT Training Programme")
+st.write("this is more text")
+
+st.markdown("# This is a Heading 1")  # Large heading
+st.markdown("## This is a Heading 2")  # Medium heading
+st.markdown("### This is a Heading 3")  # Small heading
+
+st.markdown("**Bold text** and *italic text*")  # Bold and italic text
+st.markdown("[Click here for Google](https://www.google.com)")  # Hyperlink
+st.markdown("> This is a blockquote")  # Quotation block
